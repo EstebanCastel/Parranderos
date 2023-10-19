76c1d24435b06cfce78b2ed73de374c9ba39b560
EstebanCastel <111070716+EstebanCastel@users.noreply.github.com>
2023-10-18 20:24:39 -0500

    avances 3

--- parranderos/src/main/java/uniandes/edu/co/parranderos/modelo/RolUsuario.java
+++ parranderos/src/main/java/uniandes/edu/co/parranderos/modelo/RolUsuario.java
@@ -1,5 +1,6 @@
 package uniandes.edu.co.parranderos.modelo;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -17,7 +18,8 @@ public class RolUsuario {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
-
+    
+    @Column(name="NOMBREROL")
     private String nombreRol;
 
 }
