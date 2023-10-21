a392bc76e5effed011e178d87306088d1ca9f6c0
EstebanCastel <111070716+EstebanCastel@users.noreply.github.com>
2023-10-21 12:25:36 -0500

    Ligeros cambios funcionales

--- parranderos/src/main/java/uniandes/edu/co/parranderos/controller/PlanConsumoController.java
+++ parranderos/src/main/java/uniandes/edu/co/parranderos/controller/PlanConsumoController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import uniandes.edu.co.parranderos.modelo.PlanConsumo;
+import uniandes.edu.co.parranderos.repositorio.HotelRepository;
 import uniandes.edu.co.parranderos.repositorio.PlanConsumoRepository;
 
 @Controller
@@ -15,6 +16,9 @@ public class PlanConsumoController {
 
     @Autowired
     private PlanConsumoRepository planConsumoRepository;
+
+    @Autowired
+    private HotelRepository hotelRepository;
 
     @GetMapping("/planesconsumo")
     public String planesConsumo(Model model) {
@@ -25,6 +29,7 @@ public class PlanConsumoController {
     @GetMapping("/crearplan")
     public String planForm(Model model) {
         model.addAttribute("planconsumo", new PlanConsumo());
+        model.addAttribute("hoteles", hotelRepository.findAll()); // Asumiendo que findAll() devuelve todos los hoteles
         return "planconsumoNuevo";
     }
 
@@ -38,8 +43,10 @@ public class PlanConsumoController {
     public String planEditarForm(@PathVariable("id") Long id, Model model) {
         PlanConsumo planconsumo = planConsumoRepository.findById(id).orElse(null);
         model.addAttribute("planconsumo", planconsumo);
+        model.addAttribute("hoteles", hotelRepository.findAll()); // Añadir los hoteles al modelo
         return "planconsumoEditar";
     }
+
 
     @PostMapping("/editarPlanConsumo/{id}/save")
     public String planEditarGuardar(@PathVariable("id") Long id, @ModelAttribute PlanConsumo planconsumo) {
