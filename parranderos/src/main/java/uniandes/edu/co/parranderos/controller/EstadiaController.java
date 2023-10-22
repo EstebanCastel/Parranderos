62ee4994c7b5eb26ef3fec2c9bdf72e024c8712d
EstebanCastel <111070716+EstebanCastel@users.noreply.github.com>
2023-10-22 00:05:38 -0500

    Estadia funcionando

--- parranderos/src/main/java/uniandes/edu/co/parranderos/controller/EstadiaController.java
+++ parranderos/src/main/java/uniandes/edu/co/parranderos/controller/EstadiaController.java
@@ -1,9 +1,15 @@
 package uniandes.edu.co.parranderos.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+import uniandes.edu.co.parranderos.modelo.Estadia;
 import uniandes.edu.co.parranderos.repositorio.EstadiaRepository;
 
 @Controller
@@ -15,7 +21,46 @@ public class EstadiaController {
     @GetMapping("/estadias")
     public String estadias(Model model) {
         model.addAttribute("estadias", estadiaRepository.darEstadias());
-        return "estadias";
+        return "estadias"; // La vista de lista de estadias que ya has proporcionado
+    }
+
+    @GetMapping("/crearestadia")
+    public String estadiaForm(Model model) {
+        model.addAttribute("estadia", new Estadia());
+        return "estadiaNueva"; // El nuevo archivo HTML que crearemos a continuación
+    }
+
+    @PostMapping("/crearestadia/save")
+    public String estadiaGuardar(@ModelAttribute Estadia estadia) {
+        estadiaRepository.insertarEstadia(estadia.getId(), estadia.getReserva().getId(), estadia.getCuentaConsumo().getId(), estadia.getPazYsalvo(), estadia.getCheckin(), estadia.getCheckout());
+        return "redirect:/estadias";
+    }
+
+    @GetMapping("/editarestadia/{id}")
+    public String estadiaEditarForm(@PathVariable("id") Long id, Model model) {
+        Estadia estadia = estadiaRepository.findById(id).orElse(null);
+        if (estadia != null) {
+            model.addAttribute("estadia", estadia);
+            return "estadiaEditar"; // El nuevo archivo HTML que crearemos a continuación
+        } else {
+            return "redirect:/estadias";
+        }
+    }
+
+    @PostMapping("/editarestadia/{id}/save")
+    public String estadiaEditarGuardar(@PathVariable("id") Long id, @ModelAttribute Estadia estadia) {
+        estadiaRepository.actualizarEstadia(id, estadia.getReserva().getId(), estadia.getCuentaConsumo().getId(), estadia.getPazYsalvo(), estadia.getCheckin(), estadia.getCheckout());
+        return "redirect:/estadias";
+    }
+
+    @GetMapping("/eliminarestadia/{id}")
+    public String estadiaEliminar(@PathVariable("id") long id, RedirectAttributes redirectAttrs) {
+        try {
+            estadiaRepository.eliminarEstadia(id);
+        } catch (DataIntegrityViolationException e) {
+            redirectAttrs.addFlashAttribute("errorMessage", "No se puede eliminar la estadia porque está siendo utilizado en otra tabla.");
+            return "redirect:/estadias";
+        }
+        return "redirect:/estadias";
     }
 }
-
--- parranderos/src/main/java/uniandes/edu/co/parranderos/repositorio/EstadiaRepository.java
+++ parranderos/src/main/java/uniandes/edu/co/parranderos/repositorio/EstadiaRepository.java
@@ -1,15 +1,32 @@
 package uniandes.edu.co.parranderos.repositorio;
 
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.stereotype.Repository;
-import uniandes.edu.co.parranderos.modelo.Estadia;
 import java.util.Collection;
 
-@Repository
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.transaction.annotation.Transactional;
+import uniandes.edu.co.parranderos.modelo.Estadia;
+import java.sql.Date;
+
 public interface EstadiaRepository extends JpaRepository<Estadia, Long> {
 
     @Query(value = "SELECT * FROM estadias", nativeQuery = true)
     Collection<Estadia> darEstadias();
-}
 
+    @Modifying
+    @Transactional
+    @Query(value = "INSERT INTO estadias (ID, RESERVA_ID, CUENTACONSUMO_ID, PAZYSALVO, CHECKIN, CHECKOUT) VALUES (:id, :reserva_id, :cuentaConsumo_id, :pazYsalvo, :checkin, :checkout)", nativeQuery = true)
+    void insertarEstadia(@Param("id") Long id, @Param("reserva_id") Long reserva_id, @Param("cuentaConsumo_id") Long cuentaConsumo_id, @Param("pazYsalvo") Boolean pazYsalvo, @Param("checkin") Date checkin, @Param("checkout") Date checkout);
+
+    @Modifying
+    @Transactional
+    @Query(value = "UPDATE estadias SET RESERVA_ID = :reserva_id, CUENTACONSUMO_ID = :cuentaConsumo_id, PAZYSALVO = :pazYsalvo, CHECKIN = :checkin, CHECKOUT = :checkout WHERE ID = :id", nativeQuery = true)
+    void actualizarEstadia(@Param("id") Long id, @Param("reserva_id") Long reserva_id, @Param("cuentaConsumo_id") Long cuentaConsumo_id, @Param("pazYsalvo") Boolean pazYsalvo, @Param("checkin") Date checkin, @Param("checkout") Date checkout);
+
+    @Modifying
+    @Transactional
+    @Query(value = "DELETE FROM estadias WHERE ID = :id", nativeQuery = true)
+    void eliminarEstadia(@Param("id") Long id);
+}
