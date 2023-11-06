d90747de96fe256af3158373c7cb8fac3331fd14
EstebanCastel <111070716+EstebanCastel@users.noreply.github.com>
2023-11-06 15:59:59 -0500

    piscina

--- parranderos/src/main/java/uniandes/edu/co/parranderos/controller/ServicioController.java
+++ parranderos/src/main/java/uniandes/edu/co/parranderos/controller/ServicioController.java
@@ -1,11 +1,31 @@
 package uniandes.edu.co.parranderos.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import uniandes.edu.co.parranderos.modelo.Piscina;
+import uniandes.edu.co.parranderos.repositorio.PiscinaRepository;
 
 @Controller
 public class ServicioController {
+
     @GetMapping("/servicios")
     public String mostrarServicios() {
         return "servicios";
     }
+
+    @Autowired
+    private PiscinaRepository piscinaRepository;
+    @GetMapping("/piscina")
+    public String mostrarPiscinas(Model model) {
+        List<Piscina> listaPiscinas = piscinaRepository.findAll();
+        model.addAttribute("piscinas", listaPiscinas);
+        return "piscina";
+    }
+
 }
+
