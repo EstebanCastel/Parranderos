c5c38f51102dc5b033f779bcf8c23a810d0cf240
EstebanCastel <111070716+EstebanCastel@users.noreply.github.com>
2023-11-06 16:38:08 -0500

    REQ 2 ITERAICON 2

--- parranderos/src/main/java/uniandes/edu/co/parranderos/controller/ServicioController.java
+++ parranderos/src/main/java/uniandes/edu/co/parranderos/controller/ServicioController.java
@@ -1,31 +1,50 @@
 package uniandes.edu.co.parranderos.controller;
 
+import java.sql.Date;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
-import uniandes.edu.co.parranderos.modelo.Piscina;
-import uniandes.edu.co.parranderos.repositorio.PiscinaRepository;
+import uniandes.edu.co.parranderos.repositorio.ServiciosRepository;
 
 @Controller
 public class ServicioController {
+
+    @Autowired
+    private ServiciosRepository serviciosRepository;
 
     @GetMapping("/servicios")
     public String mostrarServicios() {
         return "servicios";
     }
 
-    @Autowired
-    private PiscinaRepository piscinaRepository;
-    @GetMapping("/piscina")
-    public String mostrarPiscinas(Model model) {
-        List<Piscina> listaPiscinas = piscinaRepository.findAll();
-        model.addAttribute("piscinas", listaPiscinas);
-        return "piscina";
+    @GetMapping("/serviciosMasSolicitados")
+    public String serviciosMasSolicitados(@RequestParam("fechaInicio") Date fechaInicio, @RequestParam("fechaFin") Date fechaFin, Model model) {
+        List<Object[]> resultados = serviciosRepository.serviciosMasSolicitados(fechaInicio, fechaFin);
+        model.addAttribute("resultados", resultados);
+        return "formularioServiciosMasSolicitados"; 
+    }
+
+
+    @GetMapping("/mostrarFormularioServiciosMasSolicitados")
+    public String mostrarFormularioServiciosMasSolicitados() {
+        return "formularioServiciosMasSolicitados";  // Este sería el nombre del archivo HTML del formulario para ingresar los datos de la consulta
+    }
+
+    @GetMapping("/serviciosMenosSolicitados")
+    public String serviciosMenosSolicitados(@RequestParam("fechaInicio") Date fechaInicio, @RequestParam("fechaFin") Date fechaFin, Model model) {
+        List<Object[]> resultados = serviciosRepository.serviciosMenosSolicitados(fechaInicio, fechaFin);
+        model.addAttribute("resultados", resultados);
+        return "serviciosMenosSolicitados";  // Este sería el nombre del archivo HTML que muestra los resultados
+    }
+
+    @GetMapping("/mostrarFormularioServiciosMenosSolicitados")
+    public String mostrarFormularioServiciosMenosSolicitados() {
+        return "formularioServiciosMenosSolicitados";  // Este sería el nombre del archivo HTML del formulario para ingresar los datos de la consulta
     }
 
 }
-
