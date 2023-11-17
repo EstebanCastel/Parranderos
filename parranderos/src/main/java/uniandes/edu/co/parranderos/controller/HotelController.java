e58d1144f224f9a3c769a57c798ca022f5dcb959
EstebanCastel <111070716+EstebanCastel@users.noreply.github.com>
2023-11-17 00:39:08 -0500

    Funcionando Mongo DB

--- parranderos/src/main/java/uniandes/edu/co/parranderos/controller/HotelController.java
+++ parranderos/src/main/java/uniandes/edu/co/parranderos/controller/HotelController.java
@@ -1,16 +1,17 @@
 package uniandes.edu.co.parranderos.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 import uniandes.edu.co.parranderos.modelo.Hotel;
 import uniandes.edu.co.parranderos.repositorio.HotelRepository;
 
 import java.util.List;
 
-@RestController
+@Controller
 @RequestMapping("/hoteles")
 public class HotelController {
 
@@ -18,8 +19,9 @@ public class HotelController {
     private HotelRepository hotelRepository;
 
     @GetMapping
-    public List<Hotel> getAllHotels() {
-        return hotelRepository.findAll();
+    public String getAllHotels(Model model) {
+        List<Hotel> hotels = hotelRepository.findAll();
+        model.addAttribute("hotels", hotels);
+        return "hoteles"; // Nombre del archivo de plantilla sin la extensión .html
     }
 }
-
